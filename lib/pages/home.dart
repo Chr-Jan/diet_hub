aa800b648fdefa9d325b4f7a16e3547c4eee0310
Christoffer Jansson <christofferjansson1@gmail.com>
2024-12-17 21:19:12 +0100

    textfield under search field

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,43 +8,85 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: appBar(),
+        backgroundColor: Colors.white,
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              margin: EdgeInsets.only(top: 40, right: 20, left: 20),
-              decoration: BoxDecoration(boxShadow: [
-                BoxShadow(
-                  color: Color(0xff1D1617).withOpacity(0.11),
-                  blurRadius: 40,
-                  spreadRadius: 0.0,
-                )
-              ]),
-              child: TextField(
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding: EdgeInsets.all(15),
-                      prefixIcon: Padding(
-                        padding: const EdgeInsets.all(12),
-                        child: SvgPicture.asset(
-                          'assets/icons/search.svg',
-                          height: 20,
-                          width: 20,
-                        ),
+            _searchField(),
+            SizedBox(
+              height: 40,
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    'Category',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ));
+  }
+
+  Container _searchField() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, right: 20, left: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Color(0xff1D1617).withOpacity(0.11),
+          blurRadius: 40,
+          spreadRadius: 0.0,
+        )
+      ]),
+      child: TextField(
+          decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: 'Search Pancakes',
+              hintStyle: TextStyle(color: Colors.blue, fontSize: 14),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset(
+                  'assets/icons/search.svg',
+                  height: 20,
+                  width: 20,
+                ),
+              ),
+              suffixIcon: Container(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
                       ),
-                      suffixIcon: Padding(
+                      Padding(
                           padding: const EdgeInsets.all(12),
                           child: SvgPicture.asset(
                             'assets/icons/tune.svg',
                             height: 20,
                             width: 20,
                           )),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide.none))),
-            )
-          ],
-        ));
+                    ],
+                  ),
+                ),
+              ),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none))),
+    );
   }
 
   AppBar appBar() {
